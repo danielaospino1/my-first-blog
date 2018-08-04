c144da783389dbaf88f8179670f0d87c124f1e12
Daniela <danielaospinob15@gmail.com>
2018-08-04 10:48:26 -0500

    cambie mi usuario

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9&y0+v3=8yd5t8o1ndsy4#w60-^-(y$zts$(*s+^i7q@15v6mk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'wonderdani.pythonanywhere.com']
 
 
 # Application definition
